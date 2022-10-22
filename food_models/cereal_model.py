2e254e964d62234b7f2b29f21d28905ddfdf432c
wattow24 <adhvaithr7@gmail.com>
2022-10-22 16:49:27 -0700

    models created from first ml model

---
+++ food_models/cereal_model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, r2_score
+import matplotlib as plt
+import warnings
+from sklearn.exceptions import DataConversionWarning
+import json
+
+warnings.filterwarnings(action='ignore', category=UserWarning)
+
+
+food_data = pd.read_csv("food_data/cereals_and_bakery_products_avg_prices.csv")
+
+food_data.rename(columns={"CUSR0000SAF111":"CPI"}, inplace=True)
+
+for index, element in enumerate(food_data["DATE"]):
+    stuff = [int(x) for x in list(element.split(sep="-"))]
+    food_data["DATE"][index] = datetime(stuff[0], stuff[1], stuff[2], 0, 0).timestamp()
+
+X = food_data[["DATE"]]
+Y = food_data[["CPI"]]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.2)
+
+lr = LinearRegression()
+lr.fit(X_train, Y_train)
+
+def predict(date: str):
+    
+    date_list = [int(x) for x in list(date.split(sep="/"))]
+    epoch_time_of_date = datetime(date_list[2], date_list[0], date_list[1], 0, 0).timestamp()
+    prediction_array = np.array([[epoch_time_of_date]])
+
+    price_pred = lr.predict(prediction_array)
+    return round(float(json.loads(str(price_pred))[0][0]), 2)
+
+test = predict("9/1/2022")
+print(test)
+
