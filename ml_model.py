e8eb0f5c323c34b18176f5ec2bdef4939139dfc4
wattow24 <adhvaithr7@gmail.com>
2022-10-22 13:48:42 -0700

    ok
    Co-authored-by: EpicCodeWizard <EpicCodeWizard@users.noreply.github.com>

--- ml_model.py
+++ ml_model.py
@@ -5,6 +5,11 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, r2_score
 import matplotlib as plt
+import warnings
+from sklearn.exceptions import DataConversionWarning
+import json
+
+warnings.filterwarnings(action='ignore', category=UserWarning)
 
 
 coffee_data = pd.read_csv("CPI_Urban_US_City_avg.csv")
@@ -21,8 +26,15 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.2)
 lr = LinearRegression()
 lr.fit(X_train, Y_train)
 
+def predict(date: str):
+    
+    date_list = [int(x) for x in list(date.split(sep="/"))]
+    epoch_time_of_date = datetime(date_list[2], date_list[0], date_list[1], 0, 0).timestamp()
+    prediction_array = np.array([[epoch_time_of_date]])
 
-y_pred = lr.predict(X_test)
-accuracy = r2_score(Y_test, y_pred)
-print(accuracy)
+    price_pred = lr.predict(prediction_array)
+    return round(float(json.loads(str(price_pred))[0][0]), 2)
+
+test = predict("9/1/2022")
+print(test)
 
